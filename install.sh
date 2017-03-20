5ee2644a803181a9a97cf858eba214bb5d3a9fe1
Sander van der Heyden <sander@musonisystem.com>
2017-03-20 14:35:08 +0100

    Update Install.sh in line with Musoni prefs

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd "${tmpdir}" || exit 1
 
 # yum install -y git # if necessary
 # or download a tarball and decompress it instead
-git clone https://github.com/widdix/aws-ec2-ssh.git
+git clone https://github.com/sheyden/aws-ec2-ssh.git
 
 cd "${tmpdir}/aws-ec2-ssh" || exit 1
 
@@ -17,13 +17,13 @@ cp import_users.sh /opt/import_users.sh
 # changing GROUPNAMES to a comma seperated list of IAM groups you want to sync.
 # You can specify 1 or more groups, comma seperated, without spaces.
 # If you leave it blank, all IAM users will be synced.
-#sudo sed -i 's/IAM_AUTHORIZED_GROUPS=""/IAM_AUTHORIZED_GROUPS="GROUPNAMES"/' /opt/import_users.sh
+sudo sed -i 's/IAM_AUTHORIZED_GROUPS=""/IAM_AUTHORIZED_GROUPS="Musoni"/' /opt/import_users.sh
 
 # To control which users are given sudo privileges, uncomment the line below
 # changing GROUPNAME to either the name of the IAM group for sudo users, or
 # to ##ALL## to give all users sudo access. If you leave it blank, no users will
 # be given sudo access.
-#sudo sed -i 's/SUDOERSGROUP=""/SUDOERSGROUP="GROUPNAME"/' /opt/import_users.sh
+sudo sed -i 's/SUDOERSGROUP=""/SUDOERSGROUP="Musoni"/' /opt/import_users.sh
 
 # To control which local groups a user will get, uncomment the line belong
 # changing GROUPNAMES to a comma seperated list of local UNIX groups.
